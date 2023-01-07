619cdbd6ed15e7f5702ce444f50d1fa6927cd8e8
dingwan <dingwan0331@gmail.com>
2023-01-07 18:40:45 +0900

    feat: 회원가입 기능 구현

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+import bcrypt
+
+from django.views import View
+from django.http  import JsonResponse
+
+from apps.auth.models import User
+
+class UserView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        
+        username = data['username']
+        password = data['password']
+        email    = data['email']
+
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        
+        dto = {
+            'username': username,
+            'password': hashed_password,
+            'email' : email
+        }
+
+        print(User.objects.create(**dto))
+        return JsonResponse({})
