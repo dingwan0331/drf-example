1bcb47dcb56dbae9459ee0372cef95ea98434291
dingwan <dingwan0331@gmail.com>
2023-01-07 17:24:32 +0900

    feat: User Model 작성
    - 마이그레이션 및 마이그레이트 완료

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    password = models.BinaryField(max_length=60)
+    email    = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return f'{self.username} ({self.pk})'
