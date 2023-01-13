f47f7738bb110d6f04462564ce811b29cbe24e92
dingwan <dingwan0331@gmail.com>
2023-01-14 00:26:09 +0900

    feat: Comment모델링 추가
    - migrations 및 migrate 적용

--- apps/example/models.py
+++ apps/example/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+    email = models.EmailField()
+    content = models.CharField(max_length=200)
+    created = models.DateTimeField()
+
+    class Meta:
+        db_table = "comments"
