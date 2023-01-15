8e2eec451a6b4a64458ed99aeb6c665809a89464
dingwan <dingwan0331@gmail.com>
2023-01-15 17:07:47 +0900

    feat: CommentSerializer 작성

---
+++ apps/example/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    content = serializers.CharField(max_length=200)
+    created = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Comment.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get("email", instance.email)
+        instance.content = validated_data.get("content", instance.content)
+        instance.created = validated_data.get("created", instance.created)
+        instance.save()
+        return instance
