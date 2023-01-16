54f57801f14f4ca19dfb28bf57b8acb0f1f77ce0
dingwan <dingwan0331@gmail.com>
2023-01-16 17:50:27 +0900

    fix: 모든 함수들 전역변수를 사용하는것이 아닌 parameter값 사용하는것으로 변경

--- serializers/serializer/tests.py
+++ serializers/serializer/tests.py
@@ -10,7 +10,7 @@ valid_data = {"email": "lak@na.com", "content": "a", "created": datetime.now()}
 def call_create_with_invalid_data_as_save(data):
     try:
         # 키워드 인자로 data 하나만 넣었기때문에 save시 create메서드를 호출한다.
-        comment = CommentSerializer(data=invalid_data)
+        comment = CommentSerializer(data=data)
         print("is_valid: ", comment.is_valid())
         print("comment_errors: ", comment.errors)
         print("comment.validated_data: ", comment.validated_data)
@@ -25,7 +25,7 @@ def call_create_with_invalid_data_as_save(data):
 def call_create_with_valid_data_as_save(data):
     try:
         # 키워드 인자로 data 하나만 넣었기때문에 save시 create메서드를 호출한다.
-        comment = CommentSerializer(data=valid_data)
+        comment = CommentSerializer(data=data)
         print("is_valid: ", comment.is_valid())
         print("comment_errors: ", comment.errors)
         print("comment.validated_data: ", comment.validated_data)
@@ -40,7 +40,7 @@ def call_update_with_invalid_data_as_save(data):
     try:
         # save시 DB object와 data두가지를 인자로 넣었기에 update메서드를 호출한다.
         comment_raw = Comment.objects.last()
-        comment = CommentSerializer(comment_raw, data=invalid_data)
+        comment = CommentSerializer(comment_raw, data=data)
         print("is_valid: ", comment.is_valid())
         print("comment_errors: ", comment.errors)
         print("comment.validated_data: ", comment.validated_data)
