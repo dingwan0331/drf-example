dee66235b55c8f71211ad17fa465e73b00f59080
dingwan <dingwan0331@gmail.com>
2023-01-16 17:16:05 +0900

    feat: tests.py에 invalid_data로 save()호출한 케이스 작성(update 호출)

--- serializers/serializer/tests.py
+++ serializers/serializer/tests.py
@@ -34,3 +34,18 @@ def call_create_with_valid_data_as_save(data):
 
     except Exception as e:
         print(e)
+
+
+def call_update_with_invalid_data_as_save(data):
+    try:
+        # save시 DB object와 data두가지를 인자로 넣었기에 update메서드를 호출한다.
+        comment_raw = Comment.objects.last()
+        comment = CommentSerializer(comment_raw, data=invalid_data)
+        print("is_valid: ", comment.is_valid())
+        print("comment_errors: ", comment.errors)
+        print("comment.validated_data: ", comment.validated_data)
+        # comment의  validated_data를 인자로 update메서드를 호출한다.
+        comment.save()
+
+    except Exception as e:
+        print(e)
