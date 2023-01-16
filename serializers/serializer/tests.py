626d813d8e3c4ac3e325e67925d4af860af951f3
dingwan <dingwan0331@gmail.com>
2023-01-16 18:09:33 +0900

    refactor: create case 함수 하나로 통합
    - 동일한 로직에 인자로만 흐름이 바뀌기에 통합

--- serializers/serializer/tests.py
+++ serializers/serializer/tests.py
@@ -7,36 +7,21 @@ invalid_data = {"a": 1, "b": 2, "c": 3}
 valid_data = {"email": "lak@na.com", "content": "a", "created": datetime.now()}
 
 
-def call_create_with_invalid_data_as_save(data):
+def call_create_with_save(data):
     try:
         # 키워드 인자로 data 하나만 넣었기때문에 save시 create메서드를 호출한다.
         comment = CommentSerializer(data=data)
         print("is_valid: ", comment.is_valid())
         print("comment_errors: ", comment.errors)
         print("comment.validated_data: ", comment.validated_data)
-        # comment의  validated_data를 인자로 create메서드를 호출한다.
-        # 빈 dict이기 때문에 이경우 raise발생
-        comment.save()
+        # comment의  is_valid를 통과 못할 시 raise
+        print("comment instance: ", comment.save())
 
     except Exception as e:
         print(e)
 
 
-def call_create_with_valid_data_as_save(data):
-    try:
-        # 키워드 인자로 data 하나만 넣었기때문에 save시 create메서드를 호출한다.
-        comment = CommentSerializer(data=data)
-        print("is_valid: ", comment.is_valid())
-        print("comment_errors: ", comment.errors)
-        print("comment.validated_data: ", comment.validated_data)
-        # comment의  validated_data를 인자로 create메서드를 호출한다.
-        comment.save()
-
-    except Exception as e:
-        print(e)
-
-
-def call_update_with_invalid_data_as_save(data):
+def call_update_with_save(data):
     try:
         # save시 DB object와 data두가지를 인자로 넣었기에 update메서드를 호출한다.
         comment_raw = Comment.objects.last()
@@ -44,8 +29,8 @@ def call_update_with_invalid_data_as_save(data):
         print("is_valid: ", comment.is_valid())
         print("comment_errors: ", comment.errors)
         print("comment.validated_data: ", comment.validated_data)
-        # comment의  validated_data를 인자로 update메서드를 호출한다.
-        comment.save()
+        # comment의  is_valid를 통과 못할 시 raise
+        print("comment instance: ", comment.save())
 
     except Exception as e:
         print(e)
