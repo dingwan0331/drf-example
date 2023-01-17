0923f0fd82e27cff21f1232e3be311a93f22e765
dingwan <dingwan0331@gmail.com>
2023-01-17 17:32:30 +0900

    feat: field level validate를 구현한 시리얼라저 작성

--- serializers/serializer/serializers.py
+++ serializers/serializer/serializers.py
@@ -1,3 +1,6 @@
+import datetime
+import re
+
 from rest_framework import serializers
 from .models import Comment
 
@@ -16,3 +19,41 @@ class CommentSerializerWithOutCustomValidate(serializers.Serializer):
         instance.created = validated_data.get("created", instance.created)
         instance.save()
         return instance
+
+
+class CommentSerializerWithFieldValidate(serializers.Serializer):
+    email = serializers.CharField()
+    content = serializers.CharField(max_length=10)
+    created = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Comment.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get("email", instance.email)
+        instance.content = validated_data.get("content", instance.content)
+        instance.created = validated_data.get("created", instance.created)
+        instance.save()
+        return instance
+
+    # validate 함수들
+    # validate_<필드명> 작성시 자동으로 사용된다.
+
+    def validate_email(self, email):
+        EMAIL_REGEX = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+        if not re.fullmatch(EMAIL_REGEX, email):
+            raise serializers.ValidationError("Invalid Email")
+
+        return email
+
+    def validate_content(self, content):
+        if type(content) != str:
+            raise serializers.ValidationError
+        return content
+
+    def validate_created(self, created):
+        if type(created) != datetime.datetime:
+            raise serializers.ValidationError
+
+        return created
